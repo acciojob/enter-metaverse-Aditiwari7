88c932a46c1555adf961fdb4de1648bb8898ccfe
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2023-05-05 13:18:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 let p = document.getElementById('status');
 let btn = document.getElementById('btn');
 
-btn.addEventListener('click', () =>{
-	p.innerHtml = "";
+btn.addEventListener('click', () => {
+	p.innerHTML = "";
 	let h1 = document.createElement('h1');
 	h1.innerHTML = "Entered Metaverse";
 	p.appendChild(h1);
