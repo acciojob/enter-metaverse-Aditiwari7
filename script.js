daf0748858eed75b8155d18df6a2ae0597f45a14
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2023-05-05 13:29:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 //your JS code here. If required.
 
-let p = document.getElementById('status');
-let btn = document.getElementById('btn');
+const enterBtn = document.getElementById("enterBtn");
+const status = document.getElementById("status");
 
-btn.addEventListener('click', function() {
-	p.innerHTML = "Entered Metaverse";
-	// let h1 = document.createElement('h1');
-	// h1.innerHTML = "Entered Metaverse";
-	// p.appendChild(h1);
+enterBtn.addEventListener("click", function() {
+  status.innerHTML = "<h1>Entered Metaverse</h1>";
 });
+
+// let btn = document.getElementById('btn');
+// let status = document.getElementById('status');
+
+// btn.addEventListener('click', function() {
+// 	p.innerHTML = "Entered Metaverse";
+// 	// let h1 = document.createElement('h1');
+// 	// h1.innerHTML = "Entered Metaverse";
+// 	// p.appendChild(h1);
+// });
